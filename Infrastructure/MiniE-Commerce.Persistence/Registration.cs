f111c893d3f2411c2df801c527e71123e776897a
Nurullah Muratoğlu <83472201+nurullahmuratoglu@users.noreply.github.com>
2024-01-09 04:36:38 +0300

    Product ve categorry entityleri oluşturulurdu. migrate edildi. Write ve Read repositoryler yapıldı.

---
+++ Infrastructure/MiniE-Commerce.Persistence/Registration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MiniE_Commerce.Persistence.Context;
+using MiniE_Commorce.Application.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniE_Commerce.Persistence
+{
+    public static class Registration
+    {
+        public static void AddPersistence(this IServiceCollection services,IConfiguration configuration) 
+        {
+            services.AddDbContext<AppDbContext>(opt =>
+    opt.UseSqlServer(configuration.GetConnectionString("SQLConnection")));
+            services.AddScoped(typeof(IReadRepository<>), typeof(IReadRepository<>));
+
+
+        }
+    }
+}
