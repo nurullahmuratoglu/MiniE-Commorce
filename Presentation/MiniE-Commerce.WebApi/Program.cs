a911264d7c6ac2dd659828177a69004afd0b37b5
Nurullah Muratoğlu <83472201+nurullahmuratoglu@users.noreply.github.com>
2024-01-13 05:48:48 +0300

    Loglama Mekanizması ve global exception handler oluşturuldu.

--- Presentation/MiniE-Commerce.WebApi/Program.cs
+++ Presentation/MiniE-Commerce.WebApi/Program.cs
@@ -4,11 +4,18 @@ using MiniE_Commerce.Infrastructure;
 using MiniE_Commerce.Persistence;
 using MiniE_Commerce.Persistence.Repositories.Category;
 using MiniE_Commerce.Persistence.Repositories.Product;
+using MiniE_Commerce.WebApi.Configurations.ColumnWriter;
+using MiniE_Commerce.WebApi.Extensions;
 using MiniE_Commorce.Application;
 using MiniE_Commorce.Application.Interfaces.Repositories.Category;
 using MiniE_Commorce.Application.Interfaces.Repositories.Product;
 using Serilog;
+using Serilog.Context;
 using Serilog.Core;
+using Serilog.Events;
+using Serilog.Sinks.MSSqlServer;
+using System.Collections.ObjectModel;
+using System.Configuration;
 using System.Security.Claims;
 using System.Text;
 
@@ -42,16 +49,37 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     });
 
 
+SqlColumn sqlColumn = new SqlColumn();
+sqlColumn.ColumnName = "UserName";
+sqlColumn.DataType = System.Data.SqlDbType.NVarChar;
+sqlColumn.PropertyName = "UserName";
+sqlColumn.DataLength = 50;
+sqlColumn.AllowNull = true;
+ColumnOptions columnOpt = new ColumnOptions();
+columnOpt.Store.Remove(StandardColumn.Properties);
+columnOpt.Store.Add(StandardColumn.LogEvent);
+columnOpt.AdditionalColumns = new Collection<SqlColumn> { sqlColumn };
+
 Logger log = new LoggerConfiguration()
     .WriteTo.Console()
     .WriteTo.File("logs/log.txt")
-    
-
-
-
+    .WriteTo.MSSqlServer(
+    connectionString: builder.Configuration.GetConnectionString("SQLConnection"),
+     sinkOptions: new MSSqlServerSinkOptions
+     {
+         AutoCreateSqlTable = true,
+         TableName = "logs",
+     },
+     appConfiguration: null,
+     columnOptions: columnOpt
+    ).WriteTo.Seq(builder.Configuration["Seq:ServerURL"])
+    .Enrich.FromLogContext()
+    .Enrich.With<CustomUserNameColumn>()
+    .MinimumLevel.Information()
     .CreateLogger();
-
 builder.Host.UseSerilog(log);
+
+
 builder.Services.AddPersistence(builder.Configuration);
 builder.Services.AddAplication();
 builder.Services.AddInfrastructure();
@@ -66,12 +94,19 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.ConfigureExceptionHandler<Program>(app.Services.GetRequiredService<ILogger<Program>>());
 app.UseHttpsRedirection();
+app.UseSerilogRequestLogging();
 app.UseAuthentication();
 
 app.UseAuthorization();
 
+app.Use(async (context, next) =>
+{
+    var username = context.User?.Identity?.IsAuthenticated != null || true ? context.User.Identity.Name : null;
+    LogContext.PushProperty("UserName", username);
+    await next();
+});
 
 app.MapControllers();
 
